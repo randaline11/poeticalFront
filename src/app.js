9dfb47a68bdd8d10256d90148e58cd6bb62a8abf
Abby Starr <astarr2015@gmail.com>
2018-04-17 23:58:02 -0400

    first introduction of react

--- src/app.js
+++ src/app.js
@@ -6,36 +6,8 @@ import moment from 'moment'
 import './style.scss'
 import * as timelineFunctions from './timelineFormatService.js';
 
-// // create an array with nodes
-// var nodes = new DataSet([
-//   {id: 1, label: 'Node 1'},
-//   {id: 2, label: 'Node 2'},
-//   {id: 3, label: 'Node 3'},
-//   {id: 4, label: 'Node 4'},
-//   {id: 5, label: 'Node 5'}
-// ]);
-//
-// // create an array with edges
-// var edges = new DataSet([
-//   {from: 1, to: 3},
-//   {from: 1, to: 2},
-//   {from: 2, to: 4},
-//   {from: 2, to: 5},
-//   {from: 3, to: 3}
-// ]);
-//
-// // create a network
-// var container = document.getElementById('mynetwork');
-// var data = {
-//   nodes: nodes,
-//   edges: edges
-// };
-// var options = {
-//   interaction: {
-//     navigationButtons: true
-//   }
-// };
-// var network = new Network(container, data, options);
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 
 // var data = new DataSet([
@@ -48,34 +20,78 @@ import * as timelineFunctions from './timelineFormatService.js';
 //   {id: 7, content: 'item 7', start: moment('2013-04-17'), end: moment('2013-04-20')},
 //   {id: 8, content: 'item 8', start: moment('2013-04-19'), end: moment('2013-04-21')}
 // ]);
-var toAdd = timelineFunctions.createAllTimelineItems().then((stuff) => {
-  console.log('retrieved stuff: ', stuff);
-  var data = new DataSet(stuff);
 
-  var container = document.getElementById('visualization');
+class App extends Component {
+  // here's what our constructor would look like
+  constructor(props) {
+    super(props);
+    this.state = {
+      addTerm: '',
+    };
+  }
 
-  var groups = [
-    {
-      id: 1,
-      content: 'Group 1'
-      // Optional: a field 'className', 'style', 'order', [properties]
-    }
-    // more groups...
-  ];
+  componentDidMount() {
+    console.log('componentDidMount');
+    // firebasedb.fetchNotes((notes) => {
+    //   console.log(`fetched the notes! ${JSON.stringify(notes)}`);
+    //   this.setState({ notes: Immutable.Map(notes) });
+  //  });
+  var timeline;
+  var toAdd = timelineFunctions.createAllTimelineItems().then((stuff) => {
+    console.log('retrieved stuff: ', stuff);
+    var data = new DataSet(stuff);
 
-   var options = {
-     verticalScroll: true,
-     horizontalScroll: true,
-     zoomKey: 'ctrlKey',
-     height: 200
-   };
+    var container = document.getElementById('visualization');
 
-  var timeline = new Timeline(container, data, options);
-});
+    var groups = [
+      {
+        id: 1,
+        content: 'Group 1'
+        // Optional: a field 'className', 'style', 'order', [properties]
+      }
+      // more groups...
+    ];
 
+     var options = {
+       verticalScroll: true,
+       horizontalScroll: true,
+       zoomKey: 'ctrlKey',
+       height: 200
+     };
 
+    timeline = new Timeline(container, data, options);
+  });
 
-timeline.on('select', function (properties) {
-  const display = document.getElementById('display').innerHTML=`selected id: ${properties.items}`;
+  timeline.on('select', function (properties) {
+    const display = document.getElementById('display').innerHTML=`selected id: ${properties.items}`;
+  });
 
-});
+  }
+
+  // changeNote(id, fields) {
+  //   console.log(this);
+  //
+  //   firebasedb.changeNote(id, fields);
+  // }
+  //
+  // deleteNote(id) {
+  //   console.log(this);
+  //   firebasedb.removeNote(id);
+  // }
+  //
+  // addNote(note) {
+  //   console.log(this);
+  //   firebasedb.createNote(note);
+  // }
+  // search(text) {
+  //   this.setState({
+  //     addTerm: text,
+  //   });
+  // }
+
+  render() {
+    return <div className="test">All the React are belong to us!</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('main'));
